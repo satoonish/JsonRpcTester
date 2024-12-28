fb515ea7df48d1e937abae656d4c1235a482d266
Satsuki <satsuki200214@gmail.com>
2024-12-28 22:12:20 +0900

    [add] JsonRpc でメッセージを送る機能を追加しました

---
+++ src/JsonRpcTester.Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonRpcTester.Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("JSON-RPC サーバーを起動しました");
+            var listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync();
+                _ = Task.Run(() => HandleClientAsync(client));
+            }
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                var jsonRpc = new JsonRpc(stream, stream, new Program());
+                jsonRpc.StartListening();
+                await jsonRpc.Completion;
+            }
+        }
+
+        [JsonRpcMethod("Message")]
+        public string Message(string message)
+        {
+            Console.WriteLine($"ClientMessage: {message}");
+            return "Success";
+        }
+    }
+}
--- src/JsonRpcTester/ViewModels/MainWindowViewModel.cs
+++ src/JsonRpcTester/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
-﻿using StreamJsonRpc;
+﻿using Reactive.Bindings;
+using StreamJsonRpc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +12,36 @@ namespace JsonRpcTester.ViewModels
 {
     public class MainWindowViewModel
     {
+        public ReactiveProperty<string> SendMessage { get; }
+            = new ReactiveProperty<string>();
+
+        public AsyncReactiveCommand SendCommand { get; }
+            = new AsyncReactiveCommand();
+
+        public MainWindowViewModel()
+        {
+            SendCommand.Subscribe(ExeSendCommandAsync);
+        }
+
+        ~MainWindowViewModel()
+        {
+            SendCommand.Dispose();
+        }
+
+        private async Task ExeSendCommandAsync()
+        {
+            using (var client = new TcpClient())
+            {
+                await client.ConnectAsync("localhost", 5000);
+                using (var stream = client.GetStream())
+                {
+                    var jsonRpc = new JsonRpc(stream, stream);
+                    jsonRpc.StartListening();
+
+                    var result = await jsonRpc.InvokeAsync<string>("Message", SendMessage.Value);
+                    Console.WriteLine($"Result: {result}");
+                }
+            }
+        }
     }
 }
